64a5e242a2e3cb76bb881613cb3580097a994bae
igdev <dorultanianos@gmail.com>
2024-10-09 16:27:07 +0300

    refactor: account data initial state

--- program/src/state.rs
+++ program/src/state.rs
@@ -2,7 +2,7 @@ use borsh::{BorshDeserialize, BorshSerialize};
 
 // The last number is optional, if user does not want to the add bonus number, then it will be zero.
 pub type DraftNumbers = [u8; 7];
-pub type Guesses = Vec<DraftNumbers>;
+pub type Guesses = [DraftNumbers; 4];
 
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
 pub enum LotoInstruction {
@@ -14,5 +14,5 @@ pub enum LotoInstruction {
 
 #[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct AccountData {
-    guesses: Guesses,
+    pub guesses: Option<Guesses>,
 }

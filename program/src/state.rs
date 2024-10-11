0872d0af9d5eae6c3666abb6cf46c21add35d50c
igdev <dorultanianos@gmail.com>
2024-10-11 14:50:16 +0300

    fix: states

--- program/src/state.rs
+++ program/src/state.rs
@@ -1,18 +1,23 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 
 // The last number is optional, if user does not want to the add bonus number, then it will be zero.
+
 pub type DraftNumbers = [u8; 7];
-pub type Guesses = [DraftNumbers; 4];
+pub type Ticket = [DraftNumbers; 4];
 
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
 pub enum LotoInstruction {
-    Initialize(AccountData),
-    PurchaseTickets { guesses: Vec<DraftNumbers> },
+    Initialize(TicketAccountData),
+    PurchaseTicket { ticket: Ticket },
     SelectWinners(DraftNumbers),
     CloseAccount,
 }
 
 #[derive(Debug, BorshSerialize, BorshDeserialize)]
-pub struct AccountData {
-    pub guesses: Option<Guesses>,
+pub struct TicketAccountData {
+    // The account index, when fully filled the program will create a new account
+    pub account_index: usize,
+    // The actual tickets containing guesses, maximum 4 guesses per ticket
+    pub ticket: Ticket,
+    // The number of tickets filled, scluding the empty tickets
 }

ed0ae722cd594388d5b938fa617f615eb89f2143
igdev <dorultanianos@gmail.com>
2024-10-14 12:12:23 +0300

    feat: pool account authority

--- program/src/state.rs
+++ program/src/state.rs
@@ -20,5 +20,31 @@ pub struct TicketAccountData {
 
 #[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct PoolAccount {
-    pub referee: Pubkey,
+    // The user account which has the authority to validate tickets and airdrop prizes from the stake pool.
+    pub stake_pool_authority: Pubkey,
+    // The recipe token, used to send back a recipe depending on the amount of tickets bought, 1 token per ticket.
+    pub recipe_token: Pubkey,
+    // The user owning the recipe token.
+    pub recipe_token_owner: Pubkey,
+    // The user account wich has the authority to move funds from StakeAccount
+    // for now, this is a simple account, in the near future it will be managed by comunity votes
+    pub stake_house_authority: Pubkey,
+    // The user which has the authority to move funds from FeesAccount
+    pub fees_pool_authority: Pubkey,
+}
+
+pub enum PoolStorageSeed {
+    StakePool,
+    StakeHouse,
+    FeesPool,
+}
+
+impl PoolStorageSeed {
+    pub fn as_bytes(&self) -> &[u8] {
+        match self {
+            PoolStorageSeed::StakePool => "StakePool".as_bytes(),
+            PoolStorageSeed::StakeHouse => "StakeHouse".as_bytes(),
+            PoolStorageSeed::FeesPool => "FeesPool".as_bytes(),
+        }
+    }
 }

478e1173bc7374075d835c99a38ebe39308cf34b
igdev <dorultanianos@gmail.com>
2024-10-22 17:07:16 +0300

    tests: init stake_pool

--- program/tests/lib.rs
+++ program/tests/lib.rs
@@ -1,10 +1,9 @@
-use std::str::FromStr;
-
 use rs_merkle::{algorithms::Sha256, Hasher, MerkleTree};
+use std::str::FromStr;
 
 use solana_lottery_program::{
     processor::processor as lottery_processor,
-    state::{LotoInstruction, TicketAccountData},
+    state::{LotoInstruction, PoolStorageAccount, PoolStorageSeed, TicketAccountData},
 };
 use solana_program_test::*;
 use solana_sdk::{
@@ -18,13 +17,13 @@ use solana_sdk::{
     transaction::Transaction,
 };
 
-const RECEIPT_MINT: &str = "2WbTFMaLWnBkW8rSdZEGpFuqMncYUHhwJHjuiwvKupAg";
-const RECEIPT_MINT_AUTHORITY: &str = "CTQYF649hi9V5D3urRtRh1hqiT4efddmJVnqhGxn32wb";
+const RECEIPT_MINT: &str = "Au4Ltjubh7rSdbGqKoaPsAno6aSmggaqwBwHRP2jJEFD";
+const RECEIPT_MINT_AUTHORITY: &str = "68HumYhXUHYoUXtzbXbv6FQ9WcMMQmESJCr1zw27a9xn";
 
 #[tokio::test]
 async fn initialize_pool() {
     let program_id = Pubkey::new_unique();
-    let pool_authority = Keypair::new();
+    // let pool_authority = Keypair::new();
 
     let mut program_test = ProgramTest::new(
         "solana_lottery_program",
@@ -32,14 +31,55 @@ async fn initialize_pool() {
         processor!(lottery_processor),
     );
 
-    program_test.add_account(
-        pool_authority.pubkey(),
-        Account::new(100_000_000, 0, &program_id),
+    // program_test.add_account(
+    //     pool_authority.pubkey(),
+    //     Account::new(200_000_000, 0, &program_id),
+    // );
+    let (mut client, pool_authority, recent_blockhash) = program_test.start().await;
+
+    let acc = client.get_account(pool_authority.pubkey()).await.unwrap();
+
+    dbg!(acc);
+    let receipt_mint = Pubkey::from_str(&RECEIPT_MINT).unwrap();
+    let receipt_mint_authority = Pubkey::from_str(&RECEIPT_MINT_AUTHORITY).unwrap();
+    let (pool_storage_account_address, _bump) = Pubkey::find_program_address(
+        &[
+            PoolStorageSeed::StakePool.as_bytes(),
+            &pool_authority.pubkey().to_bytes(),
+        ],
+        &program_id,
     );
-    program_test.start().await;
+
+    let instruction_data = LotoInstruction::InitializePool(100_000_000);
+
+    let accounts = vec![
+        AccountMeta::new_readonly(pool_authority.pubkey(), true),
+        AccountMeta::new(pool_storage_account_address, false),
+        AccountMeta::new_readonly(receipt_mint, false),
+        AccountMeta::new_readonly(receipt_mint_authority, false),
+        AccountMeta::new_readonly(system_program::ID, false),
+    ];
+
+    let instruction = Instruction::new_with_borsh(program_id, &instruction_data, accounts);
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&pool_authority.pubkey()),
+        &[&pool_authority],
+        recent_blockhash,
+    );
+
+    client
+        .process_transaction_with_commitment(
+            tx,
+            solana_sdk::commitment_config::CommitmentLevel::Finalized,
+        )
+        .await
+        .expect("Unable to process data");
 }
 
 #[tokio::test]
+#[ignore = "Not ready yet"]
 async fn initialize_player_account() {
     let program_id = Pubkey::new_unique();
     let player_key = Keypair::new();

a96e891b803ad750b752ed6149345dd34e9fefe2
igdev <dorultanianos@gmail.com>
2024-11-04 16:53:38 +0200

    chore: delete unnecessary file

--- backend/src/config.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::{migrate::MigrateDatabase, pool::PoolConnection, MySql};
-use std::{env, error::Error};
-pub struct Config {
-    pub db_connection: PoolConnection<MySql>,
-}
-
-impl Config {}

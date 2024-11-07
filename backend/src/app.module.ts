43223f595bc7a26cc11fc22604b8904abf398825
igdev <dorultanianos@gmail.com>
2024-11-07 16:32:24 +0200

    feat: database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule.forRoot({isGlobal: true})],
+      useFactory: (config: ConfigService) => {
+        const DATABASE_HOST = config.get("DATABASE_HOST");
+        const DATABASE_PORT = config.get("DATABASE_PORT");
+        const DATABASE_NAME = config.get("DATABASE_NAME");
+        const DATABASE_USER = config.get("DATABASE_USER");
+        const DATABASE_PASSWORD = config.get("DATABASE_PASSWORD");
+         return {
+           type: "mysql",
+           host: DATABASE_HOST,
+           port: DATABASE_PORT,
+           database: DATABASE_NAME,
+           username: DATABASE_USER,
+           password: DATABASE_PASSWORD,
+         }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

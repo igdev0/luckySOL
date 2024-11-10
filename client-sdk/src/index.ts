1e9b6a1077c03ecce7ddf17955235f9c9e91dc1d
igdev <dorultanianos@gmail.com>
2024-11-10 20:41:23 +0200

    feat: process init fn

--- client-sdk/src/index.ts
+++ client-sdk/src/index.ts
@@ -1,4 +1,5 @@
 import * as instructions from './instructions.js';
 export * from "./helpers.js"
 export * from "./constants.js"
+export * from "./process.js"
 export default instructions;
---
+++ client-sdk/src/process.ts
@@ -0,0 +1,49 @@
+import {InitializePool} from '../dist/instructions.js';
+import {PublicKey, SYSVAR_RENT_PUBKEY, TransactionInstruction} from '@solana/web3.js';
+import {serialize} from '@dao-xyz/borsh';
+import {PROGRAM_ID, SYSTEM_PROGRAM_ID, TOKEN_PROGRAM_ID} from './constants.js';
+import {findPoolStoragePDA, findReceiptPoolMintPDA} from './helpers.js';
+
+export function processPoolInitializationInstruction(data: InitializePool, payer: PublicKey) {
+  const dataSerialized = Buffer.from(serialize(data));
+  const [poolStoragePDA] = findPoolStoragePDA(payer);
+  const [mintPDA] = findReceiptPoolMintPDA(payer);
+
+  return new TransactionInstruction({
+    data: dataSerialized,
+    programId: PROGRAM_ID,
+    keys: [
+      {
+        pubkey: payer,
+        isSigner: true,
+        isWritable: false
+      },
+      {
+        pubkey: poolStoragePDA,
+        isWritable: true,
+        isSigner: false
+      },
+      {
+        pubkey: mintPDA,
+        isWritable: true,
+        isSigner: false,
+      },
+      {
+        pubkey: SYSVAR_RENT_PUBKEY,
+        isSigner: false,
+        isWritable: false
+      },
+      {
+        pubkey: TOKEN_PROGRAM_ID,
+        isWritable: false,
+        isSigner: false
+      },
+      {
+        pubkey: SYSTEM_PROGRAM_ID,
+        isSigner: false,
+        isWritable: false,
+      }
+    ]
+  });
+
+}

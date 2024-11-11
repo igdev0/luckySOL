2ef8468e52f3ce77f7290295768aa75ed3a8d182
igdev <dorultanianos@gmail.com>
2024-11-11 16:01:39 +0200

    chore: update exports

--- client-sdk/src/index.ts
+++ client-sdk/src/index.ts
@@ -1,5 +1,4 @@
-import * as instructions from './instructions.js';
+export * from './instructions.js';
 export * from "./helpers.js"
 export * from "./constants.js"
 export * from "./process.js"
-export default instructions;

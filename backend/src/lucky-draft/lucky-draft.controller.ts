a1c509b32fb2e07b6877acae8721f8bc0c03855d
igdev <dorultanianos@gmail.com>
2024-12-01 10:14:42 +0200

    feat: find all lucky draft controller

--- backend/src/lucky-draft/lucky-draft.controller.ts
+++ backend/src/lucky-draft/lucky-draft.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { LuckyDraftService } from './lucky-draft.service';
 
 @Controller('lucky-draft')
-export class LuckyDraftController {}
+export class LuckyDraftController {
+  constructor(@Inject(LuckyDraftService) private service: LuckyDraftService) {}
+
+  @Get()
+  get(@Query('page') page = 1, @Query('limit') limit = 10) {
+    return this.service.findAll(page, limit);
+  }
+}
